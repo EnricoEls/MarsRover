8267c8f00abda0d12006c9534bace1af9741f5d5
Enrico <the.enrico.els@gmail.com>
2017-08-24 20:20:31 +0200

    added ability to read from text file

---
+++ MarsRover.App/Tests/TextFileTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.App.Tests
+{
+    [TestFixture]
+    class TextFileTests
+    {
+        [Test]
+        public void Given_Textfile_ShouldBeAbleToReadFile()
+        {
+            var textLines = TextHelper.ReadTextFile();
+            Assert.That(textLines.Count(), Is.GreaterThan(0));
+        }
+
+        [Test]
+        public void Given_StringWithSpases()
+        {
+            string inputString = "This Is A Spaced Out String";
+
+            string formattedString = TextHelper.RemoveAllSpaces(inputString);
+
+            Assert.That(formattedString, Is.EqualTo("ThisIsASpacedOutString"));
+        }
+    }
+}
